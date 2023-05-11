0d93abe69ef033e4af6c324e161e06ff95702437
Max Fan <git@max.fan>
2023-05-11 12:09:40 -0500

    Ensure that thread gets parked forever

--- src/bin/pbcopy.rs
+++ src/bin/pbcopy.rs
@@ -19,5 +19,7 @@ fn main() {
 
     // Due to https://github.com/aweinstock314/rust-clipboard/issues/28
     println!("Copied to clipboard! Quit CLI after pasting.");
-    thread::park();
+    loop {
+        thread::park();
+    }
 }
